3ddead9bcfe1976c2d62d5e0038576ba72113418
Jorge J. Caraballo <jcaraballo79@gmail.com>
2025-12-26 18:25:55 -0500

    Initial Commit

---
+++ Fundamentals/FundamentalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FundamentalsApp.swift
+//  Fundamentals
+//
+//  Created by Jorge Caraballo on 12/26/25.
+//
+
+import SwiftUI
+
+@main
+struct FundamentalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
